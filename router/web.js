251bd7f5b561f2b518950c9af10af551e4920afa
Yago <yagoabscosta@gmail.com>
2024-10-05 21:10:00 -0300

    Cards integrados ao banco, responsividade ajustada

--- router/web.js
+++ router/web.js
@@ -8,8 +8,14 @@ router.get('/', async (req, res) => {
         // Consulta para buscar os tipos de serviços
         const [servicos] = await pool.query('SELECT id, nome_servico, img_servico FROM tipo_servicos');
         
-        // Renderiza a página 'index.ejs' e passa o array 'servicos' para a view
-        res.render('index', { servicos });
+        // Consulta para buscar os parceiros
+        const [instituicoes] = await pool.query('SELECT nome_parceiro, descricao, imagem, url FROM parceiros WHERE tipo_parceiro = "institucional"');
+        
+        // Consulta para buscar os parceiros
+        const [lojas] = await pool.query('SELECT nome_parceiro, endereco, contato, imagem, url FROM parceiros WHERE tipo_parceiro = "loja"');
+
+        // Renderiza a página 'index.ejs' e passa os arrays 'servicos' e 'parceiros' para a view
+        res.render('index', { servicos, instituicoes, lojas });
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao carregar os tipos de serviços');
