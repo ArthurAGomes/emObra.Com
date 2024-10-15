dea009865a549b7dcfe233262ffaa6e3ef722fd6
DAVID JESUS DE LIMA <david.jlima@senacsp.edu.br>
2024-10-15 20:50:25 -0300

    por que você está aqui?

--- router/postagem.js
+++ router/postagem.js
@@ -6,14 +6,14 @@ const pool = require('../config/db');
 
 // Rota para receber a postagem de serviço
 router.post('/postar-servico', isAuthenticated, async (req, res) => {
-    const { descricao, tipo_servico, cep, prazo_combinar, valor, pedreiroId } = req.body;
+    const { descricao, tipo_servico, cep, prazo_combinar, valor } = req.body;
     const contratante_id = req.session.userId;
 
     console.log('User ID:', contratante_id);  // Verifique se o userId está presente
 
     const cep_final = cep && cep.trim() !== '' ? cep : req.session.cep;
 
-    if (!descricao || !tipo_servico || !valor || !prazo_combinar || !pedreiroId) {
+    if (!descricao || !tipo_servico || !valor || !prazo_combinar ) {
         return res.status(400).send('Preencha todos os campos obrigatórios.');
     }
 
@@ -22,7 +22,7 @@ router.post('/postar-servico', isAuthenticated, async (req, res) => {
     `;
 
     try {
-        await pool.query(query, [descricao, contratante_id, pedreiroId, tipo_servico, prazo_combinar, valor, cep_final]);
+        await pool.query(query, [descricao, contratante_id, tipo_servico, prazo_combinar, valor, cep_final]);
         // res.status(201).send('Serviço postado');
         res.redirect(`/perfil-contratante?message=Serviço cadastrado com sucesso`)
     } catch (err) {
